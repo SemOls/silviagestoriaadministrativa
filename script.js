ac8a2ec5877d23fab9d6c4f731c72f648cc50ac0
Glitch (glitch-hello-website) <none>
2022-06-06 20:42:46 +0000

    🐡💠 Checkpoint ./index.html:38693240/14047 ./script.js:38693240/1449 ./style.css:38693240/5075 ./.glitch-assets:38693240/3386

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+gsap.from(".navbar", {opacity:0, duration:2, delay:1, easy:"bounce", stagger:.6})
+gsap.from(".container", {x:400, duration:2, delay:3, easy:"bounce"})
+gsap.from(".one", {opacity:0, duration:2, delay:5, stagger:.6})
+gsap.from(".two", {opacity:0, duration:2, delay:7, stagger:.6})
+gsap.from(".three", {opacity:0, duration:2, delay:9, stagger:.6})
+gsap.from(".foto1", {opacity:0, x:-400, duration:3, delay:5})
+gsap.from(".foto2", {opacity:0, x:400, duration:3, delay:7})
+gsap.from(".foto3", {opacity:0, x:-400, duration:3, delay:9})
